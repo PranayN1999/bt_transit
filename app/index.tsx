bc29d611c78b14ee58c9334ca97af6bb680d4de7
Pranay Namburi <pranayn1999@gmail.com>
2024-10-11 17:55:33 -0400

    Milestone 1: home page map done

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import MapView, { Region } from 'react-native-maps';
+import { Link } from 'expo-router';
 
-export default function Index() {
+export default function Home() {
+  const initialRegion: Region = {
+    latitude: 39.1653,
+    longitude: -86.5264,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} initialRegion={initialRegion} />
+      <Link href="/routes-list">View Routes</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '90%',
+  },
+});
