d7cdce1166dc8138c8079062617ca7f8751e9d79
Pranay Namburi <pranayn1999@gmail.com>
2024-11-19 22:10:47 -0500

    Titles for each screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,22 @@
-import { Stack } from "expo-router";
-import { RoutesProvider } from "@/RoutesContext";
+import { Stack } from 'expo-router';
+import { RoutesProvider } from '@/RoutesContext';
 
 export default function RootLayout() {
   return (
     <RoutesProvider>
       <Stack>
-        <Stack.Screen name="index" />
+        <Stack.Screen
+          name="index"
+          options={{ title: 'BT Transit' }}
+        />
+        <Stack.Screen
+          name="routes-list"
+          options={{ title: 'Select Routes' }}
+        />
+        <Stack.Screen
+          name="bus-schedule"
+          options={{ title: 'Bus Schedules' }}
+        />
       </Stack>
     </RoutesProvider>
   );
